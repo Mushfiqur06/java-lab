be43f4936ec4f906001f52e7e602f51c002bc60e
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-07-24 18:07:27 +0600

    User Input and finally print out

---
+++ Lab2/Lab2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Lab2 {
+    public static void Task1() {
+        Scanner kb = new Scanner(System.in);
+
+        // Get a string from the user
+        System.out.print("Enter a String ");
+        String str = kb.next();
+        System.out.println(str);
+    }
+
+    public static void main(String[] args) {
+        Task1();
+    }
+}
